d3e68eb1885b160216a3572b9c2ea527c3c9299a
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-03-18 18:03:22 +0100

    player class

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ import numpy as np
 
 from apiCalls import *
 from direction import Direction
-import src.map as map
+import src.map as game_map
+from player import Player
 
 
 def parse_arguments():
@@ -21,18 +22,25 @@ def parse_arguments():
 def get_map(game_obj):
     map_from_server = game_obj.get("map")
     server_tile_list = map_from_server.get("tiles")
-    tiles = np.zeros((27, 9), dtype=map.Tile)
+    tiles = np.zeros((27, 9), dtype=game_map.Tile)
     for server_tile_row in server_tile_list:
         for server_tile in server_tile_row:
-            tile = map.Tile(server_tile)
+            tile = game_map.Tile(server_tile)
             tiles[tile.row, tile.column] = tile
-    return map.Map(tiles)
+    return game_map.Map(tiles)
 
 
 def main():
     args = parse_arguments()
     game_obj = init_game(args.train, args.bot_vs_bot, args.game_id, args.player_id)
     mapa = get_map(game_obj)
+    player1 = Player(game_obj.get("player1"))
+    player2 = Player(game_obj.get("player2"))
+    our_player = player1
+    enemy_player = player2
+    if player2.team_name == "xepoju":
+        enemy_player = player1
+    # TODO nas_potez(our_player, enemy_player, mapa)
     print("gameId: ", game_obj.get("gameId"))
 
 
