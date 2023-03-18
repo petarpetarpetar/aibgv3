76167f18fff01e8a78b90cdab8dc0c6ee38e3e62
Vlada <vladapopov02@gmail.com>
2023-03-18 14:38:17 +0100

    finished api calls

--- src/direction.py
+++ src/direction.py
@@ -3,8 +3,8 @@ from enum import Enum
 
 class Direction(str, Enum):
     up_left = "q"
-    up = ("w",)
-    up_right = ("e",)
-    down_right = ("d",)
-    down = ("s",)
+    up = "w"
+    up_right = "e"
+    down_right = "d"
+    down = "s"
     down_left = "a"
